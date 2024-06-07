1e0bbf2a4da2a668fab7e609f753c7e764345204
Henrique_Luan <105437749+h-luan2006@users.noreply.github.com>
2024-06-07 08:47:39 -0300

    save17232

---
+++ scipt.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa.principal");
+const caixaPerguntas = document.querySelector(".caixa.pergunta");
+const caixaAlternativa = document.querySelector(".caixa.alternativa");
+const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    { //primeiro objeto da lista de perguntas
+        enunciado:"pergunta1",
+        alternativa: ["alternativa1", "alternativa2"]
+    }.
+    { //segundo objeto da lista de perguntas
+        enunciado: "pergunta2",
+        alternativa: ["alternativa1", "alternativa2"]
+        
+    }
+]
